9cf036a56126eab860c99becdb280f4462ede90c
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2024-03-12 23:56:52 +0800

    modify ui
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- app/src/main/java/com/tc/reading/MainActivity.kt
+++ app/src/main/java/com/tc/reading/MainActivity.kt
@@ -3,27 +3,38 @@ package com.tc.reading
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.RelativeLayout
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.tc.reading.databinding.ActivityMainBinding
+import com.tc.reading.ui.dashboard.DashboardFragment
+import com.tc.reading.ui.home.HomeFragment
+import com.tc.reading.ui.me.AboutMeFragment
+import com.tc.reading.ui.notifications.NotificationsFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     companion object {
-        private const val ID_HOME = 1
-        private const val ID_EXPLORE = 2
-        private const val ID_MESSAGE = 3
-        private const val ID_NOTIFICATION = 4
-        private const val ID_ACCOUNT = 5
+        private const val ID_BOOK = 1
+        private const val ID_MOVIE = 2
+        private const val ID_DAY = 3
+        private const val ID_ME = 4
     }
+
+    private lateinit var bookFragment: HomeFragment
+    private lateinit var videoFragment: DashboardFragment
+    private lateinit var dayFragment: NotificationsFragment
+    private lateinit var aboutMeFragment: AboutMeFragment
+    private var currentFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,52 +44,80 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomBar.apply {
-            add(MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_home_black_24dp))
-            add(MeowBottomNavigation.Model(ID_EXPLORE, R.drawable.ic_dashboard_black_24dp))
-            add(MeowBottomNavigation.Model(ID_NOTIFICATION, R.drawable.ic_notifications_black_24dp))
+        bookFragment = HomeFragment();
+        videoFragment = DashboardFragment();
+        dayFragment = NotificationsFragment();
+        aboutMeFragment = AboutMeFragment();
 
-            setCount(ID_NOTIFICATION, "15")
+        supportActionBar?.title = "Books";
+        val fragmentHost = binding.root.findViewById<RelativeLayout>(R.id.fragment_host);
+
+
+        binding.bottomBar.apply {
+            add(MeowBottomNavigation.Model(ID_BOOK, R.drawable.ic_book))
+            add(MeowBottomNavigation.Model(ID_MOVIE, R.drawable.ic_movie))
+            add(MeowBottomNavigation.Model(ID_DAY, R.drawable.ic_sun))
+            add(MeowBottomNavigation.Model(ID_ME, R.drawable.ic_account))
+
+            //setCount(ID_NOTIFICATION, "15")
 
             setOnShowListener {
-                val name = when (it.id) {
-                    ID_HOME -> "HOME"
-                    ID_EXPLORE -> "EXPLORE"
-                    ID_MESSAGE -> "MESSAGE"
-                    ID_NOTIFICATION -> "NOTIFICATION"
-                    ID_ACCOUNT -> "ACCOUNT"
-                    else -> ""
+                when (it.id) {
+                    ID_BOOK -> {
+                        switchFragment(bookFragment)
+                    }
+                    ID_MOVIE -> {
+                        switchFragment(videoFragment)
+                    }
+                    ID_DAY -> {
+                        switchFragment(dayFragment)
+                    }
+                    ID_ME -> {
+                        switchFragment(aboutMeFragment)
+                    }
                 }
             }
 
             setOnClickMenuListener {
-                val name = when (it.id) {
-                    ID_HOME -> "HOME"
-                    ID_EXPLORE -> "EXPLORE"
-                    ID_MESSAGE -> "MESSAGE"
-                    ID_NOTIFICATION -> "NOTIFICATION"
-                    ID_ACCOUNT -> "ACCOUNT"
-                    else -> ""
-                }
+
             }
 
             setOnReselectListener {
                 Toast.makeText(context, "item ${it.id} is reselected.", Toast.LENGTH_LONG).show()
             }
 
-            show(ID_HOME)
+            show(ID_BOOK)
 
         }
 
-//        val navView: BottomNavigationView = binding.navView
-//
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        //switchFragment(bookFragment)
     }
+
+    private fun switchFragment(to: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction();
+        if (currentFragment == null) {
+            if (!to.isAdded) {
+                transaction.add(R.id.fragment_host, to).commit();
+            } else {
+                transaction.show(to).commit();
+            }
+            currentFragment = to;
+        } else {
+            if (currentFragment == to) {
+                if (!to.isAdded) {
+                    transaction.add(R.id.fragment_host, to).commit();
+                } else {
+                    transaction.show(to).commit();
+                }
+            } else {
+                if (!to.isAdded) {
+                    transaction.hide(currentFragment!!).add(R.id.fragment_host, to).commit();
+                } else {
+                    transaction.hide(currentFragment!!).show(to).commit();
+                }
+            }
+            currentFragment = to;
+        }
+    }
+
 }
